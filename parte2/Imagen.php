1e93d3c35d2c5d95806feac646433c4fb898de45
Laura_Tortosa <90604471+LauraTortosa@users.noreply.github.com>
2022-11-04 10:06:21 +0100

    Add files via upload

--- parte2/Imagen.php
+++ parte2/Imagen.php
@@ -4,17 +4,17 @@ class Imagen
 {
     //atributos
     private $src; //nombre
-    private $border = 0; //ancho
+    private $border; //ancho
     private $ruta_images; //ruta
 
     //metodos
-    function __construct($src, $border) {
-        $this->src = $src;
+    function __construct($src, $border = 0) {
+        $this->src = $this->ruta_images.$src;
         $this->border = $border;
     }
 
     function __toString() {
-        return "img src=" + $this->ruta_images + " border=" + $this->border + "/>";
+        return "<img src=" . $this->ruta_images . " border=" . $this->border . "/>";
 
     }
 }
---
+++ parte2/TestImage.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "Imagen.php";
+$img= new Imagen('eevee1.jpg');
+
+echo $img;
+?>
